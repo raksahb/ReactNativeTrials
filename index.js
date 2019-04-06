f38d1935eb38a6fee808cdd9c4bfb9c6960ca34a
Bhaskar Athmanathan <bhaskar.athma@gmail.com>
2019-04-05 17:02:08 -0700

    Updated index.js with RN paper example

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 /**
  * @format
  */
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "tomato",
+    accent: "yellow"
+  }
+};
 
-AppRegistry.registerComponent(appName, () => App);
+export default function Main() {
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+}
+
+AppRegistry.registerComponent(appName, () => Main);

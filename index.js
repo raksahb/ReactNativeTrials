15055cd40d963faa059e7d5f07f838435cffef57
Bhaskar Athmanathan <bhaskar.athma@gmail.com>
2019-04-05 17:26:37 -0700

    added storybook support

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 /**
  * @format
  */
-import * as React from "react";
-import { AppRegistry } from "react-native";
-import App from "./App";
-import { name as appName } from "./app.json";
-import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+// import * as React from "react";
+// import { AppRegistry } from "react-native";
+// import App from "./App";
+// import { name as appName } from "./app.json";
+// import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: "tomato",
-    accent: "yellow"
-  }
-};
+// const theme = {
+//   ...DefaultTheme,
+//   colors: {
+//     ...DefaultTheme.colors,
+//     primary: "tomato",
+//     accent: "yellow"
+//   }
+// };
 
-export default function Main() {
-  return (
-    <PaperProvider theme={theme}>
-      <App />
-    </PaperProvider>
-  );
-}
+// export default function Main() {
+//   return (
+//     <PaperProvider theme={theme}>
+//       <App />
+//     </PaperProvider>
+//   );
+// }
 
-AppRegistry.registerComponent(appName, () => Main);
+// AppRegistry.registerComponent(appName, () => Main);
+
+import StorybookUI from './storybook';
+
+export default StorybookUI;
